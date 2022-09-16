f95d85fceccd2b52bdea4761e66a336b7e56c33b
luca <luca@lucintosh.local>
2022-09-16 19:06:49 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hi luca");
+        System.out.println("Zeile 2");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
+    }
+}
